2adeff33733a856801376d581c25fb497a80ba42
Shivam <shivamgarade05@gmail.com>
2026-07-17 15:53:43 +0530

    Add script.js file with examples of primitive and reference data types

---
+++ datatypes/script.js
@@ -0,0 +1,62 @@
+// data types
+
+
+// primitive data types
+
+// String
+// var name = "shivam"
+// var age = 22
+// console.log(typeof name)
+// console.log(typeof age)
+
+// number
+// let num = -12.99;
+
+// console.log(typeof num)
+
+// bigint
+// let bigN = 23434n;
+
+// console.log(typeof bigN)
+
+
+// 4.undefined
+// let a = undefined;
+// console.log(a)
+// console.log(typeof a)
+
+
+// 5.null
+// let b = null;
+// console.log(b)
+// console.log(typeof b) - object
+
+
+// 6.symbol
+// let unique = Symbol("name")
+// console.log(typeof unique)
+
+
+// 7.boolean (true/false)
+// let flag = true
+// console.log(typeof flag)
+
+
+
+// Referance datatypes
+// not save original value only save their refrence or address
+
+
+// to save multiple value
+let a = [10,20,30,40,50,{},[],"string",true,null,undefined] // hetrogenious values can be stored here
+
+console.log(a[4])
+
+// {
+//     key:value
+// }
+
+let obj = {
+    name:"Ravi",
+    age:60
+}
