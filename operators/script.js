3211ba19b62dfa97454f98268f2c052fcd7b9fb3
Shivam <shivamgarade05@gmail.com>
2026-07-17 15:54:07 +0530

    Add index.html and script.js files with examples of arithmetic, assignment, comparison, logical, and ternary operators

---
+++ operators/script.js
@@ -0,0 +1,106 @@
+// Arithematic Operators
+
+// + - Addtion
+// - - subtraction
+// * - Multiplication
+// / - Division
+// % - Modulus
+// ** - Exponent
+// // - Floor Division
+
+// Assignment Operators
+
+// let a = 40;
+// += -= *= /= %=
+
+// a += 10;
+// a = a + 10 
+
+
+
+
+
+// Comparision Operators
+
+// == - check only value
+// === - check value and type
+// != - not equal 
+// !== - not equal strict check
+// >= - greater than equal
+// <= - less than equal
+// > - greater than
+// < - smaller than
+
+// console.log(12 == "12")
+
+
+
+
+// Logical Operators
+
+// && - and
+// true && true -> true
+// true && false -> false
+// false && true -> false
+// false && false -> false
+
+
+// || - or
+// true || true -> true
+// true || false -> true
+// false || true -> true
+// false || false -> false
+
+
+// ! - not
+// !true -> false
+// !false -> true
+
+// !! - real nature
+// console.log(!!NaN)
+
+
+// ternary Operator
+
+// condition ? true : false
+
+// 2 === "2" ? console.log("i am true") : console.log("i am false" )
+
+
+// ?? - null , undefined - ignore these two things
+// console.log(undefined ?? "name")
+// console.log(null ?? "name")
+// console.log(false ?? "name")
+
+// || - read false,0,-0,NaN,null,undefined,"",0n
+// console.log(undefined || "name")
+// console.log(NaN || "name")
+// console.log(false || "name")
+
+// typeof null
+// 'object'
+// typeof []
+// 'object'
+// typeof function(){}
+// 'function'
+// typeof Function
+// 'function'
+// typeof NaN
+// 'number'
+// typeof Object
+// 'function'
+
+
+// let a = []
+// undefined
+// console.log(a)
+// VM11448:1 []
+// undefined
+// typeof a
+// 'object'
+// a instanceof Array
+// true
+// a = {};
+// {}
+// a instanceof Object
+// true
