ffa6e04fcf8cb7f79a1d4ef227d00347ea4392c1
Shivam <shivamgarade05@gmail.com>
2026-07-17 15:54:13 +0530

    Add script.js file with greet and sum function examples

---
+++ function/script.js
@@ -0,0 +1,17 @@
+function greet(){
+    console.log('Hello how are you?')
+}
+
+// parameters and argument
+
+// dynamic function that sum two number
+
+function sum(a,b){ // a and b are the variables but in function case we call it parmater
+    return a + b;
+    // return 100
+}
+
+// sum(12,23) // 12 , 23 are the argument for the function sum
+
+let result = sum(12,23)
+console.log(result)
