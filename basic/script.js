4fbbe9ae31c27629c1e9d2fea728193473c4d809
Shivam <shivamgarade05@gmail.com>
2026-07-17 15:53:49 +0530

    Add script.js file with variable declarations and console log examples

---
+++ basic/script.js
@@ -0,0 +1,34 @@
+// console.log("Hello from javascript")
+// var a = 10;
+// var b = 20;
+// var c = 30;
+// console.log(b)
+// console.log(a)
+// console.log(c)
+
+// var a = 100;
+// var b = 20;
+
+// console.log(a,b)
+
+// var a = 100;
+// var a = 40; // this will we see last updated one
+
+// console.log(a) // which one will print
+
+// let b; // declared
+// let b = 10; // value assigned
+
+
+// console.log(b) // undefined : which is a value 
+
+// var a = 10;
+// var a = 20;
+
+// let b = 50;
+// let b = 23;// cannot re declare
+
+// console.log(b)
+
+// console.log(a)
+
